76c27a7793b38487f30ab78db3bac5b757c6845e
Matthew Bain <66839492+rocketstack-matt@users.noreply.github.com>
2025-03-03 06:44:03 +0000

    Update docs: (#236)

--- website/src/components/featuresTwo.js
+++ website/src/components/featuresTwo.js
@@ -4,7 +4,7 @@ import styles from '../pages/styles.module.css';
 
 export default function FeaturesTwo({title, description}) {
     return (
-        <div className={classnames('text--center col col--3 padding', styles.feature)}>
+        <div className={classnames('text--center col col--4 padding', styles.feature)}>
             <h3>{title}</h3>
             <div className={classnames(styles.featuresCTA)}>
                 {description}
